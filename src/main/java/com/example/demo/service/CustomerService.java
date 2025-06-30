714123b1c836cfbd70f2949f93783043c7155540
MacNikSipeykin <niksipeykin@gmail.com>
2025-06-30 11:16:51 +0300

    Add simple Spring Boot Application

---
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.CustomerDTO;
+
+public interface CustomerService {
+
+  void addCustomer(int id, String name);
+
+  CustomerDTO getCustomer(int id);
+}
